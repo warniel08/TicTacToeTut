9259db29ac301090ca498d7dd991b44dbdf1e1a9
Warner Nielsen <bwn888@gmail.com>
2017-07-13 17:37:21 -0600

    Finish writing gameplay

--- tictactoe.py
+++ tictactoe.py
@@ -76,4 +76,34 @@ while True:
 			position = player_choice(theBoard)
 			place_marker(theBoard, player1_marker, position)
 
-			
+			if win_check(theBoard, player1_marker):
+				display_board(theBoard)
+				print('Congratulations! You have won the game!')
+				game_on = False
+			else:
+				if full_board_check(theBoard):
+					display_board(theBoard)
+					print('The game is a draw!')
+					break
+				else:
+					turn = 'Player 2'
+
+		else:
+			display_board(theBoard)
+			position = player_choice(theBoard)
+			place_marker(theBoard, player2_marker, position)
+
+			if win_check(theBoard, player2_marker):
+				display_board(theBoard)
+				print('Player 2 has won!')
+				game_on = False
+			else:
+				if full_board_check(theBoard):
+					display_board(theBoard)
+					print('The game is a tie!')
+					break
+				else:
+					turn = 'Player 1'
+
+	if not replay():
+		break
