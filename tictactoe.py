7f5a6b4db9f09b65acbba684c9c37d50d474d390
Warner Nielsen <bwn888@gmail.com>
2017-07-13 17:18:00 -0600

    Create board

--- tictactoe.py
+++ tictactoe.py
@@ -1,2 +1,15 @@
 from __future__ import print_function 
 
+def display_board(board):
+	print('   |   |')
+	print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+	print('   |   |')
+	print('-----------')
+	print('   |   |')
+	print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+	print('   |   |')
+	print('-----------')
+	print('   |   |')
+	print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+	print('   |   |')
+
