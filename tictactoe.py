cc03ba7f13a915a068800b587c6494c68a8faa83
Warner Nielsen <bwn888@gmail.com>
2017-07-13 17:29:46 -0600

    Write more gameplay functions

--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,5 @@
 from __future__ import print_function 
+import random
 
 def display_board(board):
 	print('   |   |')
@@ -13,3 +14,51 @@ def display_board(board):
 	print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
 	print('   |   |')
 
+def player_input():
+	marker = ''
+	while not (marker == 'X' or marker == 'O'):
+		marker = raw_input('Player 1: Do you want to be X or O?').upper()
+
+	if marker == 'X':
+		return ('X', 'O')
+	else:
+		return('O', 'X')
+
+def place_marker(board, marker, position):
+	board[position] = marker
+
+def win_check(board, mark):
+	return((board[7] == mark and board[8] == mark and board[9] == mark) or
+	(board[4] == mark and board[5] == mark and board[6] == mark) or
+	(board[1] == mark and board[2] == mark and board[3] == mark) or
+	(board[7] == mark and board[4] == mark and board[1] == mark) or
+	(board[8] == mark and board[5] == mark and board[2] == mark) or
+	(board[9] == mark and board[6] == mark and board[3] == mark) or
+	(board[7] == mark and board[5] == mark and board[3] == mark) or
+	(board[9] == mark and board[5] == mark and board[1] == mark))
+
+def choose_first():
+	if random.randint(0, 1) == 0:
+		return 'Player 2'
+	else:
+		return 'Player 1'
+
+def space_check(board, position):
+	return board[position] == ' '
+
+def full_board_check(board):
+	for i in range(1, 10):
+		if space_check(board, i):
+			return False
+	return True
+
+def player_choice(board):
+	position = ' '
+	while position not in '1 2 3 4 5 6 7 8 9'.split() or not space_check(board, int(position)):
+		position = raw_input('Choose your next position: (1-9) ')
+	return int(position)
+
+def replay():
+	return raw_input('Do you want to play again? Enter Yes or No: ')
+
+print('Welcome to Tic Tac Toe!')
